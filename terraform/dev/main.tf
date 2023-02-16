e6c579532f5f3638cae3eb27832463cf07f5739d
y-tsuritani <ykt.tsuriya@gmail.com>
2023-02-17 01:11:49 +0900

    add module workload identity federation pool

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,7 +3,25 @@ provider "google" {
   region  = var.region
 }
 
-# 使用するサービスアカウント
+resource "google_service_account" "workload_identity_service_account" {
+  account_id   = "workload-identity-sa"
+  display_name = "workload-identity-sa"
+  description  = "Workload Identity Federation が使用するサービスアカウント"
+}
+
+module "gh_oidc" {
+  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  project_id  = var.project_id
+  pool_id     = "github-actions-pool"
+  provider_id = "github-actions-provider"
+  sa_mapping = {
+    "workload_identity_service_account" = {
+      sa_name   = google_service_account.workload_identity_service_account.id
+      attribute = "attribute.repository/y-tsuritani/github-actions-wlif"
+    }
+  }
+}
+
 resource "google_service_account" "func_service_account" {
   account_id   = "gcf-service-account"
   display_name = "gcf-service-account"
@@ -11,30 +29,24 @@ resource "google_service_account" "func_service_account" {
   description  = "Cloud Functions が使用するサービスアカウント"
 }
 
-#### Cloud Functions 関連（デプロイ用 zip ファイルの作成）
-# Cloud Functions デプロイ用にソースコード zip ファイルを作成
 data "archive_file" "source_file_zip" {
   type        = "zip"
   source_dir  = "../../applications/src"
   output_path = "../../applications/${var.function_name}.zip"
 }
 
-#### Cloud Functions 関連（デプロイ用 GCS バケット、オブジェクトの作成）
-# Cloud Functions ソースコードを格納する GCS バケットの作成
 resource "google_storage_bucket" "src_cloud_functions" {
   name     = "${var.function_name}_functions_src"
   project  = var.project_id
   location = var.region
 }
-# Cloud Functions ソースコード GCS オブジェクトの作成
+
 resource "google_storage_bucket_object" "source_object_zip" {
   name   = "${var.env}/${var.function_name}_${data.archive_file.source_file_zip.output_md5}.zip"
   bucket = google_storage_bucket.src_cloud_functions.name
   source = data.archive_file.source_file_zip.output_path
 }
 
-#### Cloud Functions 関連（各関数をデプロイ）
-# アプリケーション を Cloud Functions へデプロイ
 resource "google_cloudfunctions2_function" "function" {
   name        = var.function_name
   location    = var.region
